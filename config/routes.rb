dd0b8f4a0331afa560d008121c39fc6e327348e4
Muhammad Faizan <dev.faizan7886@gmail.com>
2023-08-31 19:21:10 +0500

    unusual changings

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,10 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
+
+
   # users routes
-=======
-  # users action routes
->>>>>>> features
+
   authenticated :user do
     root to: "users#edit_details"
   end
